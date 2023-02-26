8bf8fbe14fcdde9ba42411583a740c4965657c92
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2023-02-27 03:47:02 +0600

    First commit

---
+++ e-comerce-backend/connection.js
@@ -0,0 +1,29 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// // const mongoose = require('mongoose');
+// dotenv.config({ path: './config.env' });
+
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log('DB connection successful!'));
+
+
+// mongoose.connect(connectionStr, { useNewUrlparser: true })
+//     .then(() => console.log('connected to mongodb'))
+//     .catch(err => console.log(err));
+
+// mongoose.connection.on('error', err => {
+//     console.log(err)
+// })
+
+// 6YCrZKXOUWs59j5y
+// e-comerce-backend
+// 6YCrZKXOUWs59j5y
